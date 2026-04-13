0b3e34bca5808d93c9250d098ed4e2b97142093e
Jasperzx <kann9625@gmail.com>
2026-04-13 10:25:15 +0700

    Update ui_mc.lua

--- s/ui_mc.lua
+++ s/ui_mc.lua
@@ -291,7 +291,8 @@ return function(Config)
         local lbl = Instance.new("TextLabel", btn)
         lbl.Size, lbl.BackgroundTransparency, lbl.Text = UDim2.new(1,0,1,0), 1, tabData.n
         lbl.TextColor3, lbl.Font, lbl.TextSize = Color3.fromRGB(255,255,255), C_FONT, 18
-        lbl.TextWrapped = true
+        lbl.TextWrapped = false
+        lbl.TextXAlignment = Enum.TextXAlignment.Center
         
 
         btn.MouseButton1Click:Connect(function()
